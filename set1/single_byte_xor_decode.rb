e0f6c0ab9af7dafc7ccaab653a76d2e5da91ee41
Daniel Levenson <dleve123@gmail.com>
2015-11-24 09:47:20 -0500

    Sketch out approach for the second challenge

---
+++ set1/single_byte_xor_decode.rb
@@ -0,0 +1,7 @@
+describe 'decoding a hex encoded, single byte XORed string' do
+  # decode the cipher hex value string to byte arrays
+  # iterate through all possible values in a byte
+  # for each possible value from 0 to 255, XOR each byte in the ciphertext
+  # run a frequency analysis of the resulting candidate plaintext
+  # capture the hash generated
+end
